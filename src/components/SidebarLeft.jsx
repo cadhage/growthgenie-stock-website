7e860e6c8266680056aacb17a021c4d884ff765d
cadhage <cadhage@gmail.com>
2023-11-26 16:11:35 +0530

    removed commented code

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,3 +1,41 @@
+// const SidebarLeft = ({ handleItemClick }) => {
+//   return (
+//     <aside className="sidebar-left">
+//       {/* Menu options for stocks, mutual funds, FD */}
+//       <nav>
+//         <ul className="sidebar-menu">
+//           <li>
+//             <button
+//               className="sidebar-link"
+//               onClick={() => handleItemClick("Stokes")}
+//             >
+//               Stokes
+//             </button>
+//           </li>
+//           <li>
+//             <button
+//               className="sidebar-link"
+//               onClick={() => handleItemClick("Mutual Funds")}
+//             >
+//               Mutual Funds
+//             </button>
+//           </li>
+//           <li>
+//             <button
+//               className="sidebar-link"
+//               onClick={() => handleItemClick("Fixed Deposit")}
+//             >
+//               Fixed Deposit
+//             </button>
+//           </li>
+//           {/* Add more menu options */}
+//         </ul>
+//       </nav>
+//     </aside>
+//   );
+// };
+
+// export default SidebarLeft;
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -5,10 +43,6 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const SidebarLeft = ({ handleItemClick }) => {
   const [collapsed, setCollapsed] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
-
-  const toggleCollapse = () => {
-    setCollapsed(!collapsed);
-  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -21,57 +55,52 @@ const SidebarLeft = ({ handleItemClick }) => {
       }
     };
 
-    // Listen for window resize events
     window.addEventListener("resize", handleResize);
-
-    // Initial check on component mount
     handleResize();
 
-    // Cleanup the event listener
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <aside className={`sidebar-left${collapsed ? " collapsed" : ""}`}>
-      {isMobile && (
-        <button
-          className={`toggle-btn${collapsed ? " collapsed" : ""}`}
-          onClick={toggleCollapse}
-        >
-          <FontAwesomeIcon icon={faBars} className="menu-icon" />
-        </button>
-      )}
-      <nav className={`sidebar-menu${collapsed ? " hide-background" : ""}`}>
-        <ul>
-          <li>
-            <button
-              className="sidebar-link"
-              onClick={() => handleItemClick("Stokes")}
-            >
-              Stokes
-            </button>
-          </li>
-          <li>
-            <button
-              className="sidebar-link"
-              onClick={() => handleItemClick("Mutual Funds")}
-            >
-              Mutual Funds
-            </button>
-          </li>
-          <li>
-            <button
-              className="sidebar-link"
-              onClick={() => handleItemClick("Fixed Deposit")}
-            >
-              Fixed Deposit
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </aside>
+    !isMobile && (
+      <aside className={`sidebar-left${collapsed ? " collapsed" : ""}`}>
+        <nav className={`sidebar-menu${collapsed ? " hide-background" : ""}`}>
+          <ul className="sidebar-menu">
+            <li>
+              <button
+                className="sidebar-link"
+                onClick={() => handleItemClick("Stokes")}
+              >
+                Stokes
+              </button>
+            </li>
+            <li>
+              <button
+                className="sidebar-link"
+                onClick={() => handleItemClick("Mutual Funds")}
+              >
+                Mutual Funds
+              </button>
+            </li>
+            <li>
+              <button
+                className="sidebar-link"
+                onClick={() => handleItemClick("Fixed Deposit")}
+              >
+                Fixed Deposit
+              </button>
+            </li>
+            {/* Add more menu options */}
+          </ul>
+        </nav>
+      </aside>
+    )
   );
 };
 

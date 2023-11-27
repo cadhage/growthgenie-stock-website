505a8e288b2d9ef9ef12bdcb882a697322ef3e57
cadhage <cadhage@gmail.com>
2023-11-27 10:07:02 +0530

    function mobile menu as well

--- src/components/ClientScreen.jsx
+++ src/components/ClientScreen.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-const TopPerformingFDs = ({ handleMutualFundClick }) => {
+const ClientScreen = ({
+  handleMutualFundClick,
+  handleFixedDepositeClick,
+  handleTopStokesClick,
+}) => {
   const [isMobile, setIsMobile] = useState(false);
   const [showButtons, setShowButtons] = useState(false);
-  const [fdData, setFdData] = useState([]);
-  useEffect(() => {
-    fetchTopPerformingFD(); // Trigger the API call for Mutual Funds on component mount
-  }, [handleMutualFundClick]);
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 768) {
@@ -29,17 +29,6 @@ const TopPerformingFDs = ({ handleMutualFundClick }) => {
   const toggleButtons = () => {
     setShowButtons(!showButtons);
   };
-
-  const fetchTopPerformingFD = () => {
-    fetch("http://localhost:8080/api/topmutualfunds") // Replace with your API endpoint
-      .then((response) => response.json())
-      .then((result) => {
-        setFdData(result); // Store fetched FD data in state
-      })
-      .catch((error) => {
-        console.error("Error fetching Fixed Deposit data:", error);
-      });
-  };
   return (
     <div className="main-container">
       {isMobile && (
@@ -49,13 +38,17 @@ const TopPerformingFDs = ({ handleMutualFundClick }) => {
       )}
       {showButtons && (
         <div className={`horizontal-menu${isMobile ? " mobile-view" : ""}`}>
-          <button className="menu-item">Stokes</button>
-          <button className="menu-item" onClick={handleMutualFundClick}>
+          <button className="menu-item" onClick={() => handleTopStokesClick()}>
+            Stokes
+          </button>
+          <button className="menu-item" onClick={() => handleMutualFundClick()}>
             Mutual Funds
           </button>
-          {/* <button className="menu-item">Fixed Deposit</button> */}
-          <button className="menu-item" onClick={fetchTopPerformingFD}>
-            MFixed Deposit
+          <button
+            className="menu-item"
+            onClick={() => handleFixedDepositeClick()}
+          >
+            Fixed Deposit
           </button>
         </div>
       )}
@@ -65,4 +58,4 @@ const TopPerformingFDs = ({ handleMutualFundClick }) => {
   );
 };
 
-export default TopPerformingFDs;
+export default ClientScreen;

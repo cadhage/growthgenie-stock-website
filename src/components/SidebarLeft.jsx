31c1c83af29455109c58ce033b2d48a5891aa982
cadhage <cadhage@gmail.com>
2023-11-27 10:19:25 +0530

    miner adjustments

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,6 +1,80 @@
+// import { useState, useEffect } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+// const SidebarLeft = ({
+//   handleMutualFundClick,
+//   handleFixedDepositeClick,
+//   handleTopStokesClick,
+// }) => {
+//   const [collapsed, setCollapsed] = useState(false);
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       if (window.innerWidth <= 768) {
+//         setIsMobile(true);
+//         setCollapsed(true);
+//       } else {
+//         setIsMobile(false);
+//         setCollapsed(false);
+//       }
+//     };
+
+//     window.addEventListener("resize", handleResize);
+//     handleResize();
+
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   const toggleSidebar = () => {
+//     setCollapsed(!collapsed);
+//   };
+
+//   return (
+//     !isMobile && (
+//       <aside className={`sidebar-left${collapsed ? " collapsed" : ""}`}>
+//         <nav className={`sidebar-menu${collapsed ? " hide-background" : ""}`}>
+//           <ul className="sidebar-menu">
+//             <li>
+//               <button
+//                 className="sidebar-link"
+//                 onClick={() => handleTopStokesClick()}
+//               >
+//                 Stokes
+//               </button>
+//             </li>
+//             <li>
+//               <button
+//                 className="sidebar-link"
+//                 onClick={() => handleMutualFundClick()} // Handle Mutual Fund button click
+//               >
+//                 Mutual Funds
+//               </button>
+//             </li>
+//             <li>
+//               <button
+//                 className="sidebar-link"
+//                 onClick={() => handleFixedDepositeClick()}
+//               >
+//                 Fixed Deposit
+//               </button>
+//             </li>
+//             {/* Add more menu options */}
+//           </ul>
+//         </nav>
+//       </aside>
+//     )
+//   );
+// };
+
+// export default SidebarLeft;
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import "./SidebarLeft.css"; // Import CSS file for styling
 
 const SidebarLeft = ({
   handleMutualFundClick,
@@ -40,7 +114,7 @@ const SidebarLeft = ({
           <ul className="sidebar-menu">
             <li>
               <button
-                className="sidebar-link"
+                className="sidebar-link sidebar-button"
                 onClick={() => handleTopStokesClick()}
               >
                 Stokes
@@ -48,15 +122,15 @@ const SidebarLeft = ({
             </li>
             <li>
               <button
-                className="sidebar-link"
-                onClick={() => handleMutualFundClick()} // Handle Mutual Fund button click
+                className="sidebar-link sidebar-button"
+                onClick={() => handleMutualFundClick()}
               >
                 Mutual Funds
               </button>
             </li>
             <li>
               <button
-                className="sidebar-link"
+                className="sidebar-link sidebar-button"
                 onClick={() => handleFixedDepositeClick()}
               >
                 Fixed Deposit
